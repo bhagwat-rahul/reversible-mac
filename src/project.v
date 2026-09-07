9c877162ef79ba63e49fb248b2b1bc1fc7a426e1
Rahul Bhagwat <rahulmbme@gmail.com>
2026-09-07 23:17:06 +0530

    a little mixed signal placement magic

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_reversible_mac (
+module tt_um_bhagwat_rahul_reversible_mac (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
--- test/tb.v
+++ test/tb.v
@@ -27,7 +27,7 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
-  tt_um_reversible_mac user_project (
+  tt_um_bhagwat_rahul_reversible_mac user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
